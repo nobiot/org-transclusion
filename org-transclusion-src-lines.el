5cf551f437d8b916eec52e20eb65b562c35c1e92
Noboru Ota <me@nobiot.com>
2021-05-25 18:45:15 +0200

    add: src-options

--- org-transclusion-src-lines.el
+++ org-transclusion-src-lines.el
@@ -4,6 +4,8 @@
           #'org-transclusion-keyword-get-value-lines)
 (add-hook 'org-transclusion-get-keyword-values-functions
           #'org-transclusion-keyword-get-value-src)
+(add-hook 'org-transclusion-get-keyword-values-functions
+          #'org-transclusion-keyword-get-value-src-options)
 (add-hook 'org-transclusion-keyword-plist-to-string-functions
           #'org-transclusion-keyword-plist-to-string-src-lines)
 
@@ -18,10 +20,14 @@ Returns non-nil if check is pass."
   "Use PATH to return TC-CONTENT, TC-BEG-MKR, and TC-END-MKR.
 TODO need to handle when the file does not exist.  The logic to
 pars n-m for :lines is taken from
-`org-export--inclusion-absolute-lines' in ox.el."
+`org-export--inclusion-absolute-lines' in ox.el with one
+exception.  Instead of :lines 1-10 to exclude line 10, the logic
+below has been adjusted to include line 10.  This should be more
+intuitive when it comes to including lines of code."
   (let ((buf (find-file-noselect path))
         (src-lines (plist-get plist :lines))
-        (src-lang (plist-get plist :src)))
+        (src-lang (plist-get plist :src))
+        (src-options (plist-get plist :src-options)))
     (when buf
       (with-current-buffer buf
         (org-with-wide-buffer
@@ -36,7 +42,7 @@ pars n-m for :lines is taken from
                        (point)))
                 (end (if (zerop lend) (point-max)
                        (goto-char beg)
-                       (forward-line (1- lend))
+                       (forward-line lend) ;; include the line
                        (point)))
                 ;; Need markers here so that they can move
                 ;; when #+begin/end_src added
@@ -45,7 +51,11 @@ pars n-m for :lines is taken from
                 (content))
            (setq content
                  (concat
-                  (when src-lang (format "#+begin_src %s\n" src-lang))
+                  (when src-lang
+                    (concat
+                     (format "#+begin_src %s" src-lang)
+                     (when src-options (format " %s" src-options))
+                     "\n"))
                   (buffer-substring-no-properties beg end)
                   (when src-lang "#+end_src\n")))
            (list :tc-content content
@@ -58,25 +68,38 @@ pars n-m for :lines is taken from
   "It is a utility function used converting a keyword STRING to plist.
 It is meant to be used by `org-transclusion-get-string-to-plist'.
 It needs to be set in
-`org-transclusion-get-keyword-values-hook'."
-  (when (string-match ":lines +\\([0-9]*-[0-9]*\\)" string)
+`org-transclusion-get-keyword-values-hook'.
+Double qutations are optional \"1-10\"."
+  (when (string-match ":lines +\\(\"?[0-9]*-[0-9]*\"?\\)" string)
     (list :lines (org-strip-quotes (match-string 1 string)))))
 
 (defun org-transclusion-keyword-get-value-src (string)
   "It is a utility function used converting a keyword STRING to plist.
 It is meant to be used by `org-transclusion-get-string-to-plist'.
 It needs to be set in
-`org-transclusion-get-keyword-values-hook'."
-  (when (string-match ":src\\(?: +\\(.*\\)\\)?" string)
+`org-transclusion-get-keyword-values-hook'.
+Double qutations are optional :src \"python\"."
+  (when (string-match ":src +\\(\"?\\w*\"?\\)" string)
     (list :src (org-strip-quotes (match-string 1 string)))))
+
+(defun org-transclusion-keyword-get-value-src-options (string)
+  "It is a utility function used converting a keyword STRING to plist.
+It is meant to be used by `org-transclusion-get-string-to-plist'.
+It needs to be set in
+`org-transclusion-get-keyword-values-hook'.
+Double qutations are mandatory."
+  (when (string-match ":src-options +\"\\(.*\\)\"" string)
+    (list :src-options (org-strip-quotes (match-string 1 string)))))
 
 (defun org-transclusion-keyword-plist-to-string-src-lines (plist)
   (let ((string)
         (lines (plist-get plist :lines))
-        (src (plist-get plist :src)))
+        (src (plist-get plist :src))
+        (src-options (plist-get plist :src-options)))
     (concat string
      (when lines (format ":lines %s" lines))
-     (when src (format " :src %s" src)))))
+     (when src (format " :src %s" src))
+     (when src-options (format " :src-options \"%s\"" src-options)))))
 
 (defun org-transclusion-open-source-src-lines (&optional arg)
   "Open the source buffer of transclusion at point.
@@ -118,33 +141,34 @@ Currently it only re-aligns table with links in the content."
 This function is for non-Org text files."
   ;; Get the transclusion source's overlay but do not directly use it; it is
   ;; needed after exiting live-sync, which deletes live-sync overlays.
-  (when electric-indent-mode
-    (user-error "No live sync for src-code block when `electric-indent-mode' is on"))
-  (let* ((tc-pair (get-text-property (point) 'tc-pair))
-         (src-ov (text-clone-make-overlay
-                  (overlay-start tc-pair)
-                  (overlay-end tc-pair)
-                  (overlay-buffer tc-pair)))
-         (beg (marker-position (get-text-property (point) 'tc-beg-mkr)))
-         (end (marker-position (get-text-property (point) 'tc-end-mkr)))
-         (tc-ov)
-         (context (org-element-context))
-         (type (car context))
-         (src-ov-len (- (overlay-end src-ov) (overlay-start src-ov))))
-    ;; If the region is in src-block, get the content
-    (when (string= type "src-block")
-      (save-excursion
-        (goto-char (org-element-property :begin context))
-        (forward-line 1)
-        (setq beg (line-beginning-position))
-        (goto-char (- (org-element-property :end context)
-                      (org-element-property :post-blank context)))
-        (forward-char -1)
-        (forward-line -1)
-        (setq end (1+ (line-end-position)))))
-    (if (/= src-ov-len (- end beg))
-        (user-error "Error.  Lengths of transclusion and source are not identical")
-      (setq tc-ov (text-clone-make-overlay beg end))
-      (cons src-ov tc-ov))))
+ (when (string= "src-lines" (get-text-property (point) 'tc-type))
+   (when electric-indent-mode
+     (user-error "No live sync for src-code block when `electric-indent-mode' is on"))
+   (let* ((tc-pair (get-text-property (point) 'tc-pair))
+          (src-ov (text-clone-make-overlay
+                   (overlay-start tc-pair)
+                   (overlay-end tc-pair)
+                   (overlay-buffer tc-pair)))
+          (beg (marker-position (get-text-property (point) 'tc-beg-mkr)))
+          (end (marker-position (get-text-property (point) 'tc-end-mkr)))
+          (tc-ov)
+          (context (org-element-context))
+          (type (car context))
+          (src-ov-len (- (overlay-end src-ov) (overlay-start src-ov))))
+     ;; If the region is in src-block, get the content
+     (when (string= type "src-block")
+       (save-excursion
+         (goto-char (org-element-property :begin context))
+         (forward-line 1)
+         (setq beg (line-beginning-position))
+         (goto-char (- (org-element-property :end context)
+                       (org-element-property :post-blank context)))
+         (forward-char -1)
+         (forward-line -1)
+         (setq end (1+ (line-end-position)))))
+     (if (/= src-ov-len (- end beg))
+         (user-error "Error.  Lengths of transclusion and source are not identical")
+       (setq tc-ov (text-clone-make-overlay beg end))
+       (cons src-ov tc-ov)))))
 
 (provide 'org-transclusion-src-lines)
