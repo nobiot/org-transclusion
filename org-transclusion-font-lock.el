d4f7b2f7e9473886f6e64008dc1a808850666b0d
Noboru Ota <me@nobiot.com>
2021-08-22 19:16:14 +0200

    Add org-meta-link face to props
    Overall, the font-lock for Org-transclusion does the following:
    
    1. Apply face `org-transclusion-keyword' to #+keyword
    2. Re-applies Org's font-lock for links to the transclusion link
    3. Apply Org's face `org-meta-line' to transclusion properties"

---
+++ org-transclusion-font-lock.el
@@ -0,0 +1,45 @@
+(defface org-transclusion-keyword
+  '((((class color) (min-colors 88) (background light))
+     :foreground "#0030b4")
+    (((class color) (min-colors 88) (background dark))
+     :foreground "#34cfff")
+    (t
+     :foreground "darkgray"))
+  "Face for #+transclude keyword."
+  :group 'org-transclusion)
+
+(defun org-transclusion-font-lock-set ()
+  "Add font-lock function to Org's hook.  
+The hook is `org-font-lock-set-keywords-hook'."
+  (add-to-list 'org-font-lock-extra-keywords
+	       '(org-transclusion-fontify-meta-lines-and-blocks) 'append))
+
+(defun org-transclusion-fontify-meta-lines-and-blocks (limit)
+  "Override Org's font-lock for #+transclude keyword.
+This function does the following:
+
+1. Apply face `org-transclusion-keyword' to #+keyword
+2. Re-applies Org's font-lock for links to the transclusion link
+3. Apply Org's face `org-meta-line' to transclusion properties"
+  (let ((case-fold-search t)
+	(regexp "\\(^[ 	]*#\\+TRANSCLUDE:\\)\\(.*]]\\)?\\(.*$\\)")
+	(beg)(end)(keyword-end)(prop-beg)(prop-end))
+    (when (re-search-forward regexp limit t)
+      (setq beg (match-beginning 0))
+      (setq end (match-end 0))
+      (setq keyword-end (match-end 1))
+      (setq prop-beg (match-beginning 3))
+      (setq prop-end (match-end 3))
+      (remove-text-properties beg end
+			      '(font-lock-fontified t face org-meta-line))
+      (add-text-properties beg keyword-end
+			   '(font-lock-fontified t
+			     face org-transclusion-keyword))
+      (add-text-properties prop-beg prop-end
+			   '(font-lock-fontified t
+			     face org-meta-line))
+      (save-excursion
+	(goto-char beg)
+	(org-activate-links end)))))
+
+(add-hook 'org-font-lock-set-keywords-hook #'org-transclusion-font-lock-set)
