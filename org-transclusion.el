ecf8907a01c871b6730bea4ed52e896104d49a5f
Noboru Ota <me@nobiot.com>
2021-09-10 22:11:22 +0200

    doc: v0.2.2

--- org-transclusion.el
+++ org-transclusion.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/nobiot/org-transclusion
 ;; Keywords: org-mode, transclusion, writing
 
-;; Version: 0.2.1
+;; Version: 0.2.2
 ;; Package-Requires: ((emacs "27.1") (org "9.4"))
 
 ;; This file is not part of GNU Emacs.
