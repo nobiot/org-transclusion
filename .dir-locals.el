86ff2eaf197f974c50d179deedba3076e339d472
Noboru Ota <me@nobiot.com>
2021-12-25 15:14:57 +0100

    intrnl: update .dir-locals.el to cover org mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
-((emacs-lisp-mode
-  (fill-column . 80)
-  (indent-tabs-mode . nil)
+((nil
   (time-stamp-format . "%02d %:B %Y")
   (time-stamp-start . "modified:[       ]+\\\\?")
   (time-stamp-end . "$")
@@ -8,4 +6,7 @@
   (time-stamp-line-limit . 20)
   ;; Need this locale to be "C" or "en_US.UTF-8" or something to standardize the
   ;; time stamp with English
-  (system-time-locale . "C")))
+  (system-time-locale . "C"))
+ (emacs-lisp-mode
+  (fill-column . 80)
+  (indent-tabs-mode . nil)))
