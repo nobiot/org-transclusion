5cb94542e18722bf72a281441e944a8039b5301f
Noboru Ota <me@nobiot.com>
2022-06-12 17:52:19 +0200

    bump up version to 1.3.0

--- org-transclusion.el
+++ org-transclusion.el
@@ -17,12 +17,12 @@
 
 ;; Author:        Noboru Ota <me@nobiot.com>
 ;; Created:       10 October 2020
-;; Last modified: 27 May 2022
+;; Last modified: 12 June 2022
 
 ;; URL: https://github.com/nobiot/org-transclusion
 ;; Keywords: org-mode, transclusion, writing
 
-;; Version: 1.2.0
+;; Version: 1.3.0
 ;; Package-Requires: ((emacs "27.1") (org "9.4"))
 
 ;; This file is not part of GNU Emacs.
