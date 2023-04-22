5be23703f89225a24e820d7a66494126c7a337e2
Noboru Ota <me@nobiot.com>
2023-04-22 12:32:11 +0200

    refactor: add predicate org-transclusion-transclusion-keyword-p
    The check prior to this refactoring in `org-transclusion-check-add` is
    intended to check also the line at point is in a block such as an
    example. This is cleverly taken care of by `org-element-at-point` -- it
    tells the element at point is a block, not a keyword.

--- org-transclusion.el
+++ org-transclusion.el
@@ -17,7 +17,7 @@
 
 ;; Author:        Noboru Ota <me@nobiot.com>
 ;; Created:       10 October 2020
-;; Last modified: 28 March 2023
+;; Last modified: 22 April 2023
 
 ;; URL: https://github.com/nobiot/org-transclusion
 ;; Keywords: org-mode, transclusion, writing
@@ -779,8 +779,7 @@ off (removed)."
   (save-excursion
     (beginning-of-line)
     (let ((plist))
-      (when (string= "TRANSCLUDE"
-                     (org-element-property :key (org-element-at-point)))
+      (when (org-transclusion-transclusion-keyword-p)
         ;; #+transclude: keyword exists.
         ;; Further checking the value
         (when-let ((str (org-element-property :value (org-element-at-point))))
@@ -1335,10 +1334,8 @@ Case 1. Element at point is NOT #+transclude:
 Case 2. #+transclude inside another transclusion"
   (cond
    ;; Case 1. Element at point is NOT #+transclude:
-   ((let ((elm (org-element-at-point)))
-      (not (and (string= "keyword" (org-element-type elm))
-                (string= "TRANSCLUDE" (org-element-property :key elm)))))
-    (user-error (format "Not at a transclude keyword or transclusion in a block at point %d, line %d"
+   ((not (org-transclusion-transclusion-keyword-p))
+    (user-error (format "Not at a transclude keyword at point %d, line %d"
                         (point) (org-current-line))))
    ;; Case 2. #+transclude inside another transclusion
    ((org-transclusion-within-transclusion-p)
@@ -1346,6 +1343,12 @@ Case 2. #+transclude inside another transclusion"
                         (point) (org-current-line))))
    (t
     t)))
+
+(defun org-transclusion-transclusion-keyword-p ()
+  "Return t if the current point is on the \"#+transclusion\" line."
+  (when (string= "TRANSCLUDE"
+                 (org-element-property :key (org-element-at-point)))
+    t))
 
 (defun org-transclusion-within-transclusion-p ()
   "Return t if the current point is within a tranclusion region."
