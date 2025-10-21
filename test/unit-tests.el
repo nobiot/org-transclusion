92fd114b58a34e5056b21e6a5fb09e67f8679fbd
Andreas Matthias <andreas.matthias@gmail.com>
2025-10-21 23:17:13 +0200

    feat: Support for noweb chunks
    The new property `:noweb-chunk' transcludes a named chunk from a noweb file.
    For example
    
       #+transclude: [[./file.nw::chunk-A]] :noweb-chunk
    
    transcludes chunk `chunk-A` from file `file.nw'.

--- test/unit-tests.el
+++ test/unit-tests.el
@@ -15,4 +15,44 @@
   (should (equal (org-transclusion-org-file-p ".") nil))
   (should (equal (org-transclusion-org-file-p "") nil)))
 
+
+(ert-deftest org-transclusion-test-noweb-chunk ()
+  "Test `:noweb-chunk' property."
+  (let* ((filename-test "test-noweb-chunk.org")
+	 (filename-expected "test-noweb-chunk-detached.org")
+	 (buf-test (progn (find-file filename-test)
+			  (get-buffer filename-test)))
+	 (buf-expected (progn (find-file filename-expected)
+			      (get-buffer filename-expected))))
+    (should (equal (compare-buffer-substrings
+		    buf-test nil nil
+		    buf-expected nil nil)
+		   0))
+    ;; clean up
+    (kill-buffer buf-test)
+    (kill-buffer buf-expected)
+    (kill-buffer "test-noweb-chunk-1.nw")
+    (kill-buffer "test-noweb-chunk-2.nw")
+    (kill-buffer "test-noweb-chunk-3.nw")
+    (kill-buffer "test-noweb-chunk-4.nw")
+    ))
+
+
+(defun org-transclusion-detach-all ()
+  "Detach all transclusions.
+Used to created the `..-detached.org' files for unit testing."
+  (interactive)
+  (save-restriction
+    (let ((current-marker (move-marker (make-marker) (point)))
+          match list)
+      (widen)
+      (goto-char (point-min))
+      (while (setq match (text-property-search-forward 'org-transclusion-id))
+        (goto-char (prop-match-beginning match))
+	(org-transclusion-detach))
+      (goto-char current-marker)
+      (move-marker current-marker nil) ; point nowhere for GC
+      list)))
+
+
 ;;; unit-tests.el ends here
